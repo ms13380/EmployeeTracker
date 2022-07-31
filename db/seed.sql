3dc552595183f64c949d6f832d2f451ea76dc4b6
Michael Sullivan <104106828+ms13380@users.noreply.github.com>
2022-07-31 00:03:22 -0400

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,39 +1,16 @@
-USE employee_management_db;
 INSERT INTO department (name)
- VALUES ("IT"),
-		("Production"),
+ VALUES ("it,
+		("Product
 		("Engineering"),
 		("Accounting"),
 		("Sales");
--- SELECT * FROM department;
-
-INSERT INTO role (title, salary, department_ID) 
+INSERT INTO role (title, salary, department)
 VALUES ("Manager", 65000, 1),
-("IT Tech", 50000, 1),
-("Manager", 50000, 2),
-("Team Lead", 40000, 2),
-("Operator", 30000, 2),
-("Manager", 110000 , 3),
- ("Software Engineer", 85000, 3),
- ("Lead Engineer", 100000, 3),
-("Manager", 85000, 4),
-("Accountant", 70000, 4),
-("Manager", 100000, 5),
-("Sales Lead", 90000, 5),
-("Salesperson", 70000, 5);
--- SELECT * FROM role;
-
+("IT Dept", 5000, 1),
+("Team Lead", 4000, 2),
+("Operator", 3000, 2),
+("Manager", 11000 , 3),
+ ("Engineer", 8400, 3),
+("Accountant", 7000, 4),
+("Sales", 70000, 5);
 USE employee_management_db;
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES  ("Joel", "Iles", 1, NULL ),
-		("Theresa", "Louise", 2, 1),
-		("Mitch", "Young", 3, NULL),
-		("Duke", "Johnson", 4, 3),
-		("Josh", "Johnson", 5, 3),
-		("Leslie", "Knope", 6, NULL),
-		("Ron", "Swanson", 7, 6),
-		("April", "Ludgate",8, 6),
-        ("Donna", "Awesome", 9, NULL),
-        ("Tom", "Haverford", 10, 9);
--- SELECT * FROM employee;
